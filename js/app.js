ba895d01a8c9b87eb7c25c40091eaa34acfb395f
Beverly <chaoscgo@hotmail.com>
2024-09-04 10:51:05 -0500

    got all rows to work with message

--- js/app.js
+++ js/app.js
@@ -26,6 +26,11 @@ const messageEl = document.querySelector('#message');
 // initialize() => {
     // let solution = '';
     let inputBoardRow1 = [];
+    let inputBoardRow2 = [];
+    let inputBoardRow3 = [];
+    let inputBoardRow4 = [];
+    let inputBoardRow5 = [];
+    let inputBoardRow6 = [];
     let clicks = 0;
 //     let solLetters = [];
 //     let playerLetters = [];
@@ -49,11 +54,16 @@ const compareSolution = (solution, playerWord) => {
 
     if (solution === playerWord) {
         messageEl.textContent = 'You win!!';
-    } else if (solution !== playerWord) {
+    } else if (solution !== playerWord && clicks < 30) {
         messageEl.textContent = 'Try Again!';
-    }
-    console.log(messageEl.textContent);
+    } else if (solution !== playerWord && clicks === 30) {
+        messageEl.textContent = 'You lose!';
 }
+console.log(messageEl.textContent);
+    }
+ 
+ 
+
 
 
 const joinLetters = (inputBoardRow1, clicks) => {
@@ -61,17 +71,57 @@ const joinLetters = (inputBoardRow1, clicks) => {
         const playerWord = inputBoardRow1.join('');
        // console.log(playerWord);
         compareSolution(solution, playerWord);
+    } else if (clicks === 10) {
+        const playerWord = inputBoardRow2.join('');
+       // console.log(playerWord);
+        compareSolution(solution, playerWord);
+    } else if (clicks === 15) {
+        const playerWord = inputBoardRow3.join('');
+       // console.log(playerWord);
+        compareSolution(solution, playerWord);
+    } else if (clicks === 20) {
+        const playerWord = inputBoardRow4.join('');
+       // console.log(playerWord);
+        compareSolution(solution, playerWord);
+    } else if (clicks === 25) {
+        const playerWord = inputBoardRow5.join('');
+       // console.log(playerWord);
+        compareSolution(solution, playerWord);
+    } else if (clicks === 30) {
+        const playerWord = inputBoardRow6.join('');
+       // console.log(playerWord);
+        compareSolution(solution, playerWord);
     }
 }
 
 const placeLetter = (content, clicks) => {
+        if (clicks < 6) {
         inputBoardRow1.push(content);
         boardRowEls[clicks-1].textContent = content;
         // console.log(inputBoardRow1);
         joinLetters(inputBoardRow1, clicks);
-        // render();
+        } else if (clicks > 5 && clicks < 11) {
+            inputBoardRow2.push(content);
+            boardRowEls[clicks-1].textContent = content;
+            joinLetters(inputBoardRow2, clicks);
+        } else if (clicks > 10 && clicks < 16) {
+            inputBoardRow3.push(content);
+            boardRowEls[clicks-1].textContent = content;
+            joinLetters(inputBoardRow3, clicks);
+        } else if (clicks > 15 && clicks < 21) {
+            inputBoardRow4.push(content);
+            boardRowEls[clicks-1].textContent = content;
+            joinLetters(inputBoardRow4, clicks);
+        } else if (clicks > 20 && clicks < 26) {
+            inputBoardRow5.push(content);
+            boardRowEls[clicks-1].textContent = content;
+            joinLetters(inputBoardRow5, clicks);
+        } else if (clicks > 25 && clicks < 31) {
+            inputBoardRow6.push(content);
+            boardRowEls[clicks-1].textContent = content;
+            joinLetters(inputBoardRow6, clicks);
     }
-
+}
 
 
 
